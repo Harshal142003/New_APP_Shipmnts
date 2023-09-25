12608b291492e0a792cfb386ce35cd3a9040bdcf
Harshal <mistrydenish14@gmail.com>
2023-09-25 22:30:29 +0530

    "done"

--- JS/for_location.js
+++ JS/for_location.js
@@ -7,38 +7,34 @@ const options = {
 };
 
 const getWeatherByGeolocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
 
-        fetch(
-          `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?lat=${lat}&lon=${lon}`,
-          options
-        )
-          .then((response) => response.json())
-          .then((response) => {
-            console.log(response);
+      fetch(
+        `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?lat=${lat}&lon=${lon}`,
+        options
+      )
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
 
-            temp.innerHTML = response.temp;
-            humidity.innerHTML = response.humidity;
-            min_temp.innerHTML = response.min_temp;
-            max_temp.innerHTML = response.max_temp;
-            wind_speed.innerHTML = response.wind_speed;
-            sunrise.innerHTML = response.sunrise;
-            sunset.innerHTML = response.sunset;
-          })
-          .catch((err) => console.error(err));
-      },
-      (error) => {
-        console.error("Geolocation error:", error);
-      }
-    );
-  } else {
-    console.error("Geolocation is not supported by this browser.");
-  }
+          document.getElementById("temp").innerHTML = response.temp;
+          document.getElementById("humidity").innerHTML = response.humidity;
+          document.getElementById("min_temp").innerHTML = response.min_temp;
+          document.getElementById("max_temp").innerHTML = response.max_temp;
+          document.getElementById("wind_speed").innerHTML = response.wind_speed;
+          document.getElementById("sunrise").innerHTML = response.sunrise;
+          document.getElementById("sunset").innerHTML = response.sunset;
+        })
+        .catch((err) => console.error(err));
+    },
+    (error) => {
+      console.error("Geolocation error:", error);
+    }
+  );
 };
 
 getWeatherByGeolocation();
--- JS/script.js
+++ JS/script.js
@@ -17,13 +17,13 @@ const getWeather = (city) => {
     .then((response) => {
       console.log(response);
 
-      temp.innerHTML = response.temp;
-      humidity.innerHTML = response.humidity;
-      min_temp.innerHTML = response.min_temp;
-      max_temp.innerHTML = response.max_temp;
-      wind_speed.innerHTML = response.wind_speed;
-      sunrise.innerHTML = response.sunrise;
-      sunset.innerHTML = response.sunset;
+      document.getElementById("temp").innerHTML = response.temp;
+      document.getElementById("humidity").innerHTML = response.humidity;
+      document.getElementById("min_temp").innerHTML = response.min_temp;
+      document.getElementById("max_temp").innerHTML = response.max_temp;
+      document.getElementById("wind_speed").innerHTML = response.wind_speed;
+      document.getElementById("sunrise").innerHTML = response.sunrise;
+      document.getElementById("sunset").innerHTML = response.sunset;
     })
     .catch((err) => console.error(err));
 };
